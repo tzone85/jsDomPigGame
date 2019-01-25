5641aeb91eed5569b8caf937163973bb76c77496
thando mini <tzone85@gmail.com>
2019-01-25 21:45:58 +0200

    removed the white space

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
-
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
