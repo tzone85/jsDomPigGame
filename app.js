b098f80761d7b97e74c4f2f1a4ffad9eff622101
thando mini <tzone85@gmail.com>
2019-01-26 23:30:29 +0200

    made cahnges to the desplay elements of the DOM as the players play consecutively

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 
@@ -35,6 +35,31 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
     
     // 3. Update the round score if the rolled number was not a 1
+
+    if (dice !== 1) {
+        // add score
+        roundScore += dice;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+    } else {
+        // next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // changing the active class to the player that is actually playing consecutively
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+
+
+    }
 });  // the btn is called by the event listenner hence called a callback function
 
 // an unnonimous function cannot be re-used
